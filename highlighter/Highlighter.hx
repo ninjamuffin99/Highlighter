50eedd95a41bf2332fc50965a19f84d1a22edfe3
Jens Fischer <jensfischer95@gmail.com>
2020-04-14 17:07:04 +0200

    Make it usable

--- highlighter/Highlighter.hx
+++ highlighter/Highlighter.hx
@@ -1,10 +1,13 @@
 package highlighter;
 
-import Sys.exit;
+import js.html.DOMParser;
+import js.html.NodeList;
+import js.html.Element;
+import js.html.Node;
+import haxe.extern.EitherType;
+import js.html.HTMLDocument;
 import haxe.io.Bytes;
 import haxe.io.BytesInput;
-import haxe.io.BytesOutput;
-import haxe.io.Output;
 import haxe.io.Path;
 import haxe.xml.Parser.XmlParserException;
 import highlighter.VscodeTextmate;
@@ -13,261 +16,109 @@ import sys.io.File;
 
 using StringTools;
 
-class Highlighter
-{
-	static function println (output:Output, message:String)
-	{
-		output.writeString(message);
-		output.writeString("\n");
+class Highlighter {
+	static final jsdom:Dynamic = {
+		var jsdom = js.Lib.require("jsdom").JSDOM;
+		var j = js.Syntax.construct(jsdom);
+		j;
+	};
+	static final DOMParser:DOMParser = js.Syntax.construct(jsdom.window.DOMParser);
+
+	public static function main() {
+		var folder = Sys.args()[0];
+		var grammar = Sys.args()[1];
+		patchFolder(folder, ["haxe" => new Highlighter(grammar, "dark")], function(cls) return "haxe");
+		// new Highlighter(grammar, "dark").runCss();
 	}
 
-	static function usage (output:Output)
-	{
-		println(output, "node bin/highlighter.js css [--theme=light|dark|/path/to/file]");
-		println(output, "node bin/highlighter.js highlight --grammar=/path/to/file --input=stdin|file [--file=/path/to/file] [--theme=light|dark|/path/to/file]");
-	}
-
-	public static function main ()
-	{
-		// Process args
-		var args = Sys.args();
-		var cout = Sys.stdout();
-		var cerr = Sys.stderr();
-
-		if (args.length == 0)
-		{
-			usage(cout);
-			exit(0);
-		}
-		else if (args.length < 1)
-		{
-			println(cerr, "Missing command");
-			usage(cerr);
-			exit(1);
-		}
-
-		var command = args.shift();
-
-		if (command != "css" && command != "highlight")
-		{
-			println(cerr, 'Unknown command "${command}"');
-			usage(cerr);
-			exit(1);
-		}
-
-		var grammar = "";
-		var theme = "light";
-		var input = "";
-		var file = "";
-
-		for (arg in args)
-		{
-			var cut = arg.indexOf("=");
-
-			if (cut == -1)
-			{
-				println(cerr, 'Unknown argument: "${arg}"');
-				exit(1);
-			}
-
-			var name = arg.substring(2, cut);
-			var value = arg.substr(cut + 1);
-
-			switch (name)
-			{
-				case "grammar":
-					grammar = value;
-
-				case "theme":
-					theme = value;
-
-				case "input":
-					if (value == "stdin" || value == "file")
-					{
-						input = value;
-					}
-					else
-					{
-						println(cerr, 'Unknown value for input "${value}", should be either stdin or file');
-						exit(1);
-					}
-
-				case "file":
-					file = value;
-
-				default:
-					println(cerr, 'Unknown argument "${name}"');
-					exit(1);
-			}
-		}
-
-		// Validate args
-		if (command == "highlight" && input == "")
-		{
-			println(cerr, "You need to specify an input mode when highlighting");
-			exit(1);
-		}
-
-		if (command == "highlight" && grammar == "")
-		{
-			println(cerr, "You need to specify a grammar when highlighting");
-			exit(1);
-		}
-
-		if (command == "highlight" && !FileSystem.exists(grammar))
-		{
-			println(cerr, 'Grammar file "${grammar}" doesn\'t exist');
-			exit(1);
-		}
-
-		if (theme == "light")
-		{
-			theme = "light_plus.json";
-		}
-
-		if (theme == "dark")
-		{
-			theme = "dark_plus.json";
-		}
-
-		if (theme != "light_plus.json" && theme != "dark_plus.json" && !FileSystem.exists(theme))
-		{
-			println(cerr, 'Theme file "${theme}" doesn\'t exist');
-			exit(1);
-		}
-
-		if (input == "file" && file == "")
-		{
-			println(cerr, "You need to specify a file");
-			exit(1);
-		}
-
-		if (input == "file" && !FileSystem.exists(file))
-		{
-			println(cerr, 'Input file "${file}" doesn\'t exist');
-			exit(1);
-		}
-
-		// Run it
-		var h = new Highlighter(grammar, theme);
-
-		if (command == "css")
-		{
-			cout.writeString(h.runCss());
-		}
-		else if (input == "stdin")
-		{
-			cout.writeString(h.runStdin());
-		}
-		else
-		{
-			cout.writeString(h.runFile(file));
-		}
-	}
-
-	var registry : Registry;
-	var grammar : IGrammar;
-	var theme : Theme.ThemeData;
+	var registry:Registry;
+	var grammar:IGrammar;
+	var theme:Theme.ThemeData;
 
 	/**
-	Create a highlighter.
+		Create a highlighter.
 
-	@param grammar The path to the grammar file.
-	@param theme The path to the theme.
+		@param grammar The path to the grammar file.
+		@param theme The path to the theme.
 	**/
-	public function new (grammar:String, theme:String = "light")
-	{
+	public function new(grammar:String, theme:String = "light") {
 		this.registry = new Registry();
 
-		if (grammar != "")
-		{
+		if (grammar != "") {
 			this.grammar = registry.loadGrammarFromPathSync(grammar);
 		}
 
 		this.theme = Theme.load(theme);
-		this.registry.setTheme({ name: this.theme.name, settings: this.theme.tokenColors });
+		this.registry.setTheme({name: this.theme.name, settings: this.theme.tokenColors});
 	}
 
 	/**
-	Get the CSS for the theme.
+		Get the CSS for the theme.
 	**/
-	public function runCss () : String
-	{
-		var cout = new BytesOutput();
-		println(cout, CSS.generateStyle(registry));
-		return cout.getBytes().toString();
+	public function runCss() {
+		trace(CSS.generateStyle(registry));
 	}
 
 	/**
-	Run the highlighter on the stdin.
+		Run the highlighter on the stdin.
 	**/
-	public function runStdin () : String
-	{
+	public function runStdin():String {
 		var input = new BytesInput(Bytes.ofString(NodeUtils.readAllStdin()));
 		return Code.generateHighlighted(grammar, input);
 	}
 
 	/**
-	Run the highlighter on some content.
+		Run the highlighter on some content.
 
-	@param content The content to highlight.
+		@param content The content to highlight.
 	**/
-	public function runContent (content:String) : String
-	{
+	public function runContent(content:String):String {
 		var input = new BytesInput(Bytes.ofString(content));
 		return Code.generateHighlighted(grammar, input);
 	}
 
 	/**
-	Run the highlighter on some file.
+		Run the highlighter on some file.
 
-	@param content The content to highlight.
+		@param content The content to highlight.
 	**/
-	public function runFile (path:String) : String
-	{
+	public function runFile(path:String):String {
 		var input = File.read(path, false);
 		return Code.generateHighlighted(grammar, input);
 	}
 
 	/**
-	Patch the code blocks of a HTML file.
+		Patch the code blocks of a HTML file.
 
-	@param path The path of the file to patch.
-	@param grammars The available grammars.
-	@param getLang A function used to go from css class list to grammar name.
+		@param path The path of the file to patch.
+		@param grammars The available grammars.
+		@param getLang A function used to go from css class list to grammar name.
 	**/
-	public static function patchFile (path:String, grammars:Map<String, Highlighter>, getLang:String->String) : Array<String>
-	{
-		try
-		{
-			var xml = Xml.parse(File.getContent(path));
+	public static function patchFile(path:String, grammars:Map<String, Highlighter>, getLang:String->String):Array<String> {
+		try {
+			var document = DOMParser.parseFromString(File.getContent(path), TEXT_HTML);
 			var missing = new Map<String, Bool>();
 
-			processNode(grammars, getLang, xml, missing);
+			processNode(grammars, getLang, document, missing);
 
-			var result = ~/&amp;([a-z]+;)/g.replace(xml.toString(), "&$1");
+			var serializer = js.Syntax.construct(jsdom.window.XMLSerializer);
+			var result = serializer.serializeToString(document);
+			// var result = ~/&amp;([a-z]+;)/g.replace(document.conte, "&$1");
 			File.saveContent(path, result);
 
 			var a = [];
-			for (k in missing.keys())
-			{
-				if (k != "")
-				{
+			for (k in missing.keys()) {
+				if (k != "") {
 					a.push(k);
 				}
 			}
 			return a;
-		}
-		catch (e:Dynamic)
-		{
-			if (Std.is(e, XmlParserException))
-			{
+		} catch (e:Dynamic) {
+			if (Std.is(e, XmlParserException)) {
 				var e = cast(e, XmlParserException);
 				Sys.println('${e.message} at line ${e.lineNumber} char ${e.positionAtLine}');
 				Sys.println(e.xml.substr(e.position - 20, 40));
-			}
-			else
-			{
+			} else {
 				Sys.println(e);
 			}
 
@@ -277,103 +128,81 @@ class Highlighter
 	}
 
 	/**
-	Patch the code blocks of HTML files in a directory.
+		Patch the code blocks of HTML files in a directory.
 
-	@param path The path of the directory to patch.
-	@param grammars The available grammars.
-	@param getLang A function used to go from css class list to grammar name.
-	@param recursive If the patching should enter the subdirectories.
+		@param path The path of the directory to patch.
+		@param grammars The available grammars.
+		@param getLang A function used to go from css class list to grammar name.
+		@param recursive If the patching should enter the subdirectories.
 	**/
-	public static function patchFolder (path:String, grammars:Map<String, Highlighter>, getLang:String->String, recursive:Bool = true) : Array<String>
-	{
+	public static function patchFolder(path:String, grammars:Map<String, Highlighter>, getLang:String->String, recursive:Bool = true):Array<String> {
 		var missing = new Map<String, Bool>();
 
-		for (entry in FileSystem.readDirectory(path))
-		{
+		for (entry in FileSystem.readDirectory(path)) {
 			var entry_path = Path.join([path, entry]);
 
-			if (FileSystem.isDirectory(entry_path))
-			{
-				if (recursive)
-				{
+			if (FileSystem.isDirectory(entry_path)) {
+				if (recursive) {
 					var folder_missing = patchFolder(entry_path, grammars, getLang, true);
 
-					for (k in folder_missing)
-					{
+					for (k in folder_missing) {
 						missing.set(k, true);
 					}
 				}
-			}
-			else if (Path.extension(entry_path) == "html")
-			{
+			} else if (Path.extension(entry_path) == "html") {
 				var file_missing = patchFile(entry_path, grammars, getLang);
-				
-				for (k in file_missing)
-				{
+
+				for (k in file_missing) {
 					missing.set(k, true);
 				}
 			}
 		}
 
 		var a = [];
-		for (k in missing.keys())
-		{
-			if (k != "")
-			{
+		for (k in missing.keys()) {
+			if (k != "") {
 				a.push(k);
 			}
 		}
 		return a;
 	}
 
-	static function processNode (grammars:Map<String, Highlighter>, getLang:String->String, xml:Xml, missingGrammars:Map<String, Bool>)
-	{
-		if (xml.nodeType == Xml.Element)
-		{
-			switch (xml.nodeName)
-			{
+	static function processNode(grammars:Map<String, Highlighter>, getLang:String->String, document:EitherType<Node, HTMLDocument>,
+			missingGrammars:Map<String, Bool>) {
+		if (Std.is(document, jsdom.window.Node)) {
+			var node:Node = cast document;
+			switch (node.nodeName.toLowerCase()) {
 				case "pre":
-					var code = xml.firstChild();
+					var code = node.firstChild;
 
-					if (code.nodeType != Xml.Element)
-					{
+					if (code == null || code.nodeType != Node.ELEMENT_NODE) {
 						return;
 					}
 
-					var lang = code.exists("class") ? getLang(code.get("class")) : "";
+					var code:Element = cast code;
+					var lang = code.hasAttribute("class") ? getLang(code.getAttribute("class")) : "";
 
-					if (grammars.exists(lang))
-					{
-						var original = code.firstChild().toString().htmlUnescape();
+					if (grammars.exists(lang)) {
+						var original = code.firstChild.textContent.htmlUnescape();
 						var highlighted = grammars.get(lang).runContent(original);
-						var new_xml = Xml.parse(highlighted);
-						var siblings = [for (n in xml.parent) n];
-						xml.parent.insertChild(new_xml, siblings.indexOf(xml));
-						xml.parent.removeChild(xml);
-					}
-					else
-					{
+						var newNode = DOMParser.parseFromString(highlighted, TEXT_XML).firstChild;
+						node.parentNode.replaceChild(newNode, node);
+					} else {
 						missingGrammars.set(lang, true);
 					}
 
 				default:
-					processChildren(grammars, getLang, xml, missingGrammars);
+					processChildren(grammars, getLang, node.childNodes, missingGrammars);
 			}
-		}
-
-		if (xml.nodeType == Xml.Document)
-		{
-			processChildren(grammars, getLang, xml, missingGrammars);
+		} else {
+			var document:HTMLDocument = cast document;
+			processChildren(grammars, getLang, document.childNodes, missingGrammars);
 		}
 	}
 
-	static function processChildren (grammars:Map<String, Highlighter>, getLang:String->String, xml:Xml, missingGrammars:Map<String, Bool>)
-	{
-		var children = [for (n in xml) n];
-
-		for (element in children)
-		{
-			processNode(grammars, getLang, element, missingGrammars);
+	static function processChildren(grammars:Map<String, Highlighter>, getLang:String->String, nodeList:NodeList, missingGrammars:Map<String, Bool>) {
+		for (node in nodeList) {
+			processNode(grammars, getLang, node, missingGrammars);
 		}
 	}
 }
